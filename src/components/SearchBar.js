bffe4059b473a8c20faca1c8bdac1575c97d6512
Ava <36146839+ava-ly@users.noreply.github.com>
2024-08-17 21:37:22 +0700

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,7 +6,6 @@ function SearchBar({ onSubmit }) {
     const handleFormSubmit = (event) => {
         event.preventDefault();
         onSubmit(term);
-        setTerm('');
     }
 
     const handleChange = (event) => {
